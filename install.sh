216453a8b71cfd721081de5e01fca2cfc081af1b
GabePoel <gabrielpoel@gmail.com>
2020-08-08 11:46:24 -0700

    Reduced super user requirements and made loading default profile easier.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo cp ./hblock /usr/local/bin
-sudo cp ./blocker.py /usr/local/bin
-sudo chmod 744 /usr/local/bin/blocker.py
-sudo chmod 744 /usr/local/bin/hblock
+cp ./hblock /home/*/.local/bin/hblock
+cp ./hblock /usr/local/bin/hblock
+chmod +x /home/*/.local/bin/hblock
+chmod +x /usr/local/bin/hblock
 { # try
     sudo ln -s /usr/bin/python3 /usr/bin/python
 } || {
     echo "Python already set up."
 }
-sudo blocker.py setup
+sudo hblock setup
