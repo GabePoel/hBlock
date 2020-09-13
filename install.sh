aa5700cfff63bd34634a2ccf6bb508808f99ba58
GabePoel <gabrielpoel@gmail.com>
2020-09-13 11:52:01 -0700

    Better list making.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 CWD=$(pwd)
 cp $CWD/hblock /home/*/.local/bin/
+cp $CWD/block-keywords /home/*/.local/bin/
 sudo cp $CWD/hblock /usr/local/bin/
+sudo cp $CWD/block-keywords /usr/local/bin/
 chmod +x /home/*/.local/bin/hblock
 sudo chmod +x /usr/local/bin/hblock
 { # try
